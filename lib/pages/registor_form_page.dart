501e43324bed35dfeb7b47e34419c6463c836dec
unknown <rahmatjonovm0309@gmail.com>
2022-09-29 23:12:18 +0500

    first massage: the have been created about form

---
+++ lib/pages/registor_form_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class RegisterFormPage extends StatefulWidget {
+  const RegisterFormPage({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterFormPage> createState() => _RegisterFormPageState();
+}
+
+class _RegisterFormPageState extends State<RegisterFormPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Register Form'),
+        centerTitle: true,
+      ),
+      body: Form(
+        child: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: [TextField(), TextFormField()],
+        ),
+      ),
+    );
+  }
+}
